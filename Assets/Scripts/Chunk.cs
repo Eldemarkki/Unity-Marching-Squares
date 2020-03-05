94abbd9771310cc6912b397936d2577cbefdab95
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2020-03-05 17:53:32 +0200

    Added a world script

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,50 @@
+﻿using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Chunk : MonoBehaviour
+{
+    private NativeArray<float> _densities;
+
+    private MeshFilter meshFilter;
+    private Vector2Int coordinate;
+    private int chunkSize;
+    private float isolevel;
+
+    private float2 noiseOffset;
+
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    private void Start()
+    {
+        _densities = new NativeArray<float>((chunkSize + 1) * (chunkSize + 1), Allocator.Persistent);
+        for (int y = 0; y < chunkSize + 1; y++)
+        {
+            for (int x = 0; x < chunkSize + 1; x++)
+            {
+                int index = y * (chunkSize + 1) + x;
+                float density = noise.snoise((new float2(x, y) + noiseOffset) * 0.1f);
+                _densities[index] = density;
+            }
+        }
+
+        meshFilter.sharedMesh = MarchingSquares.CreateMesh(_densities, chunkSize, isolevel);
+    }
+
+    private void OnDestroy()
+    {
+        _densities.Dispose();
+    }
+
+    public void Initialize(Vector2Int chunkCoordinate, int chunkSize, float isolevel, float2 noiseOffset)
+    {
+        coordinate = chunkCoordinate;
+        this.chunkSize = chunkSize;
+        this.isolevel = isolevel;
+        this.noiseOffset = (Vector2)transform.position;
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    [Header("World settings")]
+    [SerializeField] private int worldWidth = 2;
+    [SerializeField] private int worldHeight = 2;
+
+    [Header("Chunk settings")]
+    [Range(1, 64), SerializeField] private int chunkSize = 16;
+    [SerializeField] private Chunk chunkPrefab;
+
+    [Header("Marching Cubes settings")]
+    [Range(-1f, 1f), SerializeField] private float isolevel;
+
+    private Dictionary<Vector2Int, Chunk> chunks;
+
+    private void Awake()
+    {
+        chunks = new Dictionary<Vector2Int, Chunk>(worldWidth * worldHeight);
+    }
+
+    private void Start()
+    {
+        for (int x = 0; x < worldWidth; x++)
+        {
+            for (int y = 0; y < worldHeight; y++)
+            {
+                Vector2Int chunkCoordinate = new Vector2Int(x, y);
+
+                Chunk chunk = Instantiate(chunkPrefab, (Vector2)chunkCoordinate * chunkSize, Quaternion.identity, transform);
+                chunk.Initialize(chunkCoordinate, chunkSize, isolevel, Vector2.zero);
+                chunks.Add(chunkCoordinate, chunk);
+            }
+        }
+    }
+}
